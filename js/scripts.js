4956d21a4b80f6785cf46c76efc6076496bc2cc1
chrisxkoch <chrxkoch@icloud.com>
2019-08-16 13:42:25 +0200

    Hope you like it, Chris

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// Start
+
+alert('Hello world');
+
+// RECOMMENDED
+
+var myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+// simple addition
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+// simple multiplication
+var size = 100;
+var doubleSize = size * 2;
+document.write(doubleSize);
+
+// simple math chain
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+// simple practice
+
+var favoriteFood = 'Halloumi';
+document.write(favoriteFood);
